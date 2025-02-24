d8a761a42b4ed9a6ad5ba13eecb2c4bd940e3132
Nyck-dev <gnycole947@gmail.com>
2025-02-23 21:51:23 -0300

    excluindo biblioteca

--- aulas/JAVASCRIPT/sorteiaNumero.js
+++ aulas/JAVASCRIPT/sorteiaNumero.js
@@ -1,13 +1,1 @@
-const inquirer = require("@inquirer/prompts");
 
-
-// const numAleatorio = parseInt(Math.random() * 10);
-
-// console.log(numAleatorio);
-
-async function main() {
-    const numero = await inquirer.input({ message: "Digite um número:" });
-    console.log(`Você digitou: ${numero}`);
-  }
-  
-  main();
